0839d23ef699282df959fc28d4c42a0b96b78ec7
Vivek Sahu <viveksahu1762@gmail.com>
2025-01-31 16:22:05 +0530

    bit changes in nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,10 +4,15 @@ import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <div className="relative w-full">
+    <div className="fixed w-full">
       <div className="mx-auto max-w-7xl">
-        <div className="relative px-4">
-          <Button className="absolute right-4 top-4 rounded-2xl px-6 py-6 text-sm">
+        <div className="relative px-4 flex items-center justify-between">
+          <div className="flex items-center gap-2 p-4">
+            <img src="/icons/logo.svg" className="w-16" />
+            <p className="-mt-4 text-xl font-medium">yapster.ai</p>
+          </div>
+
+          <Button className="rounded-2xl px-6 py-6 text-sm">
             Connect Wallet
           </Button>
         </div>
