ab86e5f3e9700cea468de9b6d8f90399fca9a83b
Vivek Sahu <viveksahu1762@gmail.com>
2025-01-31 13:53:02 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  manifest: "/manifest.json",
+};
+
+const Page = () => {
+  return (
+    <div className="w-full min-h-screen grid place-items-center bg-white dark:bg-gray-900">
+      <h1 className="text-2xl leading-tight tracking-tighter sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-medium text-gray-900 dark:text-gray-100">
+        Work in Progress!
+      </h1>
+    </div>
+  );
+};
+
+export default Page;
