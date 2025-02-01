c34b804f4752d5cbfdb5082c4143ccc856ab5f8d
Vivek Sahu <viveksahu1762@gmail.com>
2025-02-01 14:09:36 +0530

    done with landing page

--- src/app/fucking-testing/page.js
+++ src/app/fucking-testing/page.js
@@ -1,6 +1,11 @@
+"use client";
+import Actions from "@/components/actions";
+import Footer from "@/components/footer";
 import Info from "@/components/info";
 import Landing from "@/components/landing";
+import MainAction from "@/components/main-action";
 import Navbar from "@/components/navbar";
+import { TweetGridDemo } from "@/components/tweet-demo";
 
 export default function Page() {
   return (
@@ -9,6 +14,10 @@ export default function Page() {
       <Landing />
       <div className="hidden md:block">
         <Info />
+        <Actions />
+        <TweetGridDemo />
+        <MainAction />
+        <Footer />
       </div>
     </div>
   );
